d90507e17d4aeb70c6dad005784ad04476499d18
Karol Grochulski <116535961+kjgrochulski@users.noreply.github.com>
2026-04-19 22:33:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
-const PORT = 8000
-const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const createCsvWriter = require('csv-writer').createObjectCsvWriter
 
-const app = express()
-
-app.get('/', (reg,res) => { 
-   res.json('Welcome to my Messier Object scraper') 
+const csvWriter = createCsvWriter({
+    path: 'messier_data.csv',
+    header: [
+        { id: 'messierId', title: 'Messier Number' },
+        { id: 'ngcId', title: 'NGC Number' },
+        { id: 'objectType', title: 'Object Type' },
+        { id: 'distance', title: 'Distance (kly)' },
+        { id: 'magnitude', title: 'Apparent Magnitude' }
+    ]
 })
 
-app.get('/Wiki', (req,res) => {
-    axios.get('https://en.wikipedia.org/wiki/List_of_Messier_objects', {
-        headers: {
-            'User-Agent': 'MessierScraperProject/1.0 (learning to scrape)'
+const url = 'https://en.wikipedia.org/wiki/List_of_Messier_objects'
+
+axios.get(url, {
+    headers: {
+        'User-Agent': 'MessierProject'
+    }
+})
+.then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const scrapedData = []
+
+    //cleaner 
+    const cleanText = (text) => text.replace(/\[.*?\]/g, '').replace(/\n/g, '').trim()
+
+    $('table.wikitable tbody tr').each(function () {
+        const columns = $(this).find('td')
+
+        if (columns.length > 0) {
+            
+            const messierId = cleanText($(columns[0]).text())
+            const ngcId = cleanText($(columns[1]).text())
+            const objectType = cleanText($(columns[3]).text())
+            const distance = cleanText($(columns[4]).text())
+            const magnitude = cleanText($(columns[6]).text())
+
+            scrapedData.push({
+                messierId: messierId,
+                ngcId: ngcId,
+                objectType: objectType,
+                distance: distance,
+                magnitude: magnitude
+            })
         }
     })
-    .then((response) => {
-        const html = response.data
-        console.log('Successfully fetched the HTML')
-        const $ = cheerio.load(html)
 
-        $('a:contains("')
+    console.log(`Successfully parsed ${scrapedData.length} rows of data.`)
 
-        res.json('Check terminal for HTML')
-    
-    })
-    .catch((error) => {
-        console.error("Scraping error:", error.message)
-        res.status(500).json('Something went wrong')
-    })
+    csvWriter.writeRecords(scrapedData)
+        .then(() => {
+            console.log('Success! Your messier_data.csv file has been created.')
+        })
 })
-
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+.catch(error => {
+    console.error("Scraping error:", error.message)
+})
